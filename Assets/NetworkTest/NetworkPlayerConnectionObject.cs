21893903ab88e5853d7277b873eae71ff372c555
unknown <kadkins3211@gmail.com>
2018-02-01 14:36:14 -0800

    Simple network test

---
+++ Assets/NetworkTest/NetworkPlayerConnectionObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkPlayerConnectionObject : NetworkBehaviour {
+
+    public GameObject playerPrefab;
+	// Use this for initialization
+	void Start () {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        CmdSpawnMyPlayer();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Network commands
+    [Command]
+    void CmdSpawnMyPlayer()
+    {
+        // On the server, instantiate
+        GameObject go = Instantiate(playerPrefab);
+
+        // Send to all clients and set up networkidentity, set authority
+        NetworkServer.SpawnWithClientAuthority(go,connectionToClient);
+    }
+}
--- Assets/Scripts/Tile Scripts/RiverTile.cs
+++ Assets/Scripts/Tile Scripts/RiverTile.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 using UnityEngine.Tilemaps;
 
 public class RiverTile : Tile
